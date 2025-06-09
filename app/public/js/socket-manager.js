bd57bda27ec3588dcaab186b272aed04202a5936
ImFardad <fardadgoodboy@gmail.com>
2025-06-09 12:11:10 +0000

    hmmm

--- app/public/js/socket-manager.js
+++
@@ -1,7 +0,0 @@
-// این ماژول فقط یک بار اتصال سوکت را برقرار کرده و آن را در اختیار همه قرار می‌دهد.
-window.socket = io();
-
-// برای اطمینان از اتصال، یک لاگ در کنسول مرورگر چاپ می‌کنیم.
-window.socket.on('connect', () => {
-  console.log('Successfully connected to Socket.IO server with ID:', window.socket.id);
-});
